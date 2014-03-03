1d3add3feb6c9f028eee381bb4572da127766f27
Kevin O'Hagan <kevinohagan@gmail.com>
2014-03-03 16:41:23 +0100

    init

---
+++ errors_list.js
@@ -0,0 +1,15 @@
+Template.meteorErrors.helpers({
+    errors: function() {
+        return Meteor.errors.find();
+    }
+});
+Template.meteorError.rendered = function() {
+    var error = this.data;
+    Meteor.defer(function() {
+        Meteor.errors.update(error._id, {
+            $set: {
+                seen: true
+            }
+        });
+    });
+};
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+    summary: "Showing you app's error messages with semantic-ui"
+});
+Package.on_use(function(api, where) {
+    api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+    api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+});
+// Package.on_test(function(api) {
+//     api.use('errors', 'client');
+//     api.use(['tinytest', 'test-helpers'], 'client');
+//     api.add_files('errors_tests.js', 'client');
+// });
